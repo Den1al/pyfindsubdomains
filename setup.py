82572774da3ce9c576bb2dd5ffdcc5b048219c45
Daniel Abeles <dabeles@akamai.com>
2018-06-07 14:17:16 +0300

    renamed:    examples/driver.py -> findsubdomains/cli.py 	modified:   setup.py

--- setup.py
+++ setup.py
@@ -2,16 +2,22 @@ import os
 from setuptools import setup
 
 
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+
 setup(
-    name = "pyfindsubdomains",
-    version = "0.0.1",
-    author = "@Daniel_Abeles",
-    author_email = "abeles22@gmail.com",
-    description = "This is a python3 wrapper to FindSubDomains",
-    license = "BSD",
-    keywords = "FindSubDomains, bruteforce, dns, subdomain",
+    name="pyfindsubdomains",
+    version="0.0.2.3",
+    author="@Daniel_Abeles",
+    author_email="abeles22@gmail.com",
+    description="This is a python3 wrapper to FindSubDomains",
+    license="BSD",
+    keywords="FindSubDomains, bruteforce, dns, subdomain",
     url='https://github.com/Den1al/pyfindsubdomains',
     packages=['findsubdomains'],
+    long_description=readme(),
     classifiers=[
         "Programming Language :: Python",
         "Programming Language :: Python :: 3.6",
@@ -26,7 +32,10 @@ setup(
         "Topic :: System :: Networking :: Monitoring",
     ],
     install_requires=[
-          'requests',
-          'bs4'
-      ],
+        'requests',
+        'bs4'
+    ],
+    entry_points={
+        'console_scripts': ['findsubdomains=findsubdomains.cli:main'],
+    }
 )
